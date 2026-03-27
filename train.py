3d1283b8d852c48814664352308f8c77df543b87
Bryan Pruneda <pruneda.bryan@gmail.com>
2026-03-27 14:56:16 -0500

    Fixing deployment for ArgoCD

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
@@ -24,3 +25,24 @@ if accuracy > 0.90:
     # This is where you'd use a GitHub Token to update 'deployment.yaml'
     # For now, let's just print the 'Magic Command'
     print("NEXT STEP: Update deployment.yaml value to 'v2'")
+=======
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestRegressor
+
+# Tell Python where the 'Logbook' (MLflow) is
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("My_First_Experiment")
+
+with mlflow.start_run():
+    # Simulate training a model
+    model = RandomForestRegressor(n_estimators=100)
+    
+    # Log 'Settings' (Parameters)
+    mlflow.log_param("num_trees", 100)
+    
+    # Log 'Results' (Metrics)
+    mlflow.log_metric("accuracy", 0.95)
+    
+    print("Run complete! Check your browser at http://localhost:5000")
+>>>>>>> 5ddbc422 (Fix duplicate deployment and add missing selector)
